7e77db90b052ce6ca87c74a36a7ea8dcf5bebcf6
Premprakash Yadav <premprakash.yadav@finouxad.com>
2018-04-16 18:36:45 +0530

    saved event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MyApp latest Again';
   months = ['Jan' , 'Feb' , 'March' , 'April' , 'May' , 'June' , 'July' , 'Aug' , 'Sep', 'Oct', 'Nov' , 'Dec'];
+  isVisible = false;
+  isVisible1 = true;
+  showContent(event) {
+    console.log(event);
+
+  }
+  changeDropdown(event) {
+    console.log(event.target.value);
+    console.log('all months');
+  }
 }
